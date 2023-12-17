3ca38e7283a275101b52b264d56d8f7d0a2ff5de
Luca Pacitto <lucapacitto.dev@gmail.com>
2023-12-17 14:57:21 +0100

    :hammer: refactory: validiban's refactory

--- src/ValidIban.php
+++ src/ValidIban.php
@@ -3,7 +3,9 @@
 namespace Nembie\IbanRule;
 
 use Closure;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
 
 class ValidIban implements ValidationRule
 {
@@ -12,19 +14,22 @@ class ValidIban implements ValidationRule
      *
      * @var array
      */
-    static $countryRules = null;
+    protected static $countryRules;
 
     /**
      * The validator instance.
      *
-     * @var \Illuminate\Contracts\Validation\Validator
+     * @var Validator
      */
     protected $validator;
 
     /**
      * Run the validation rule.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): PotentiallyTranslatedString  $fail
+     * @return void
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
@@ -37,7 +42,8 @@ class ValidIban implements ValidationRule
     /**
      * Set the validator instance.
      *
-     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @param  Validator  $validator
+     * @return void
      */
     public function setValidator($validator): void
     {
@@ -45,57 +51,67 @@ class ValidIban implements ValidationRule
     }
 
     /**
-     * Validate IBAN
-     * @param string $iban
+     * Validate IBAN.
+     *
+     * @param  string  $iban
      * @return bool
      */
     protected function validateIban($iban): bool
     {
-        $hasError = false;
-
         // Check if IBAN contains white space or special characters
-        if (preg_match('/\s|[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $iban))
+        if (preg_match('/\s|[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $iban)) {
             return false;
-    
-        // Get the validation rules by country
-        if (self::$countryRules === null) {
-            $path = dirname(__FILE__, 2) . '/resources/json/countries.json';
-            $json = file_get_contents($path);
-            $countryRules = json_decode($json, true);
         }
-        
+
+        $countryRules = $this->loadCountryRules();
+
         $countryCode = substr($iban, 0, 2);
         $countryObj = $countryRules['sepa'][$countryCode] ?? $countryRules['not_sepa'][$countryCode] ?? null;
-        if ($countryObj === null)
+
+        if ($countryObj === null) {
             return false;
-    
+        }
+
         // Get validation rules
         $rules = array_map(fn($attr) => $attr[1], $countryObj);
-    
+
         // Validate IBAN against rules
         $tempIban = $iban;
         $ibanLength = 0;
+
         foreach ($rules as $rule) {
             $numbers = intval(preg_replace('/[^0-9]/', '', $rule));
             $letter = preg_replace('/[^a-zA-Z]/', '', $rule);
             $checkString = substr($tempIban, 0, $numbers);
             $ibanLength += $numbers;
-    
+
             // Check if the string part is of the correct type
-            if (
-                ($letter === 'a' && !ctype_alpha($checkString))
-                || ($letter === 'n' && !ctype_digit($checkString))
-            ) {
-                $hasError = true;
-                break;
+            if (($letter === 'a' && !ctype_alpha($checkString)) || ($letter === 'n' && !ctype_digit($checkString))) {
+                return false;
             }
-    
+
             $tempIban = substr($tempIban, $numbers);
         }
-    
-        if (!$hasError && $ibanLength != strlen($iban))
-            $hasError = true;
-    
-        return !$hasError;
-    }    
+
+        return $ibanLength == strlen($iban);
+    }
+
+    /**
+     * Load country rules.
+     *
+     * @return array
+     */
+    protected function loadCountryRules(): array
+    {
+        if (self::$countryRules === null) {
+            self::$countryRules = json_decode(
+                file_get_contents(
+                    dirname(__FILE__, 2) . '/resources/json/countries.json'
+                ),
+                true
+            );
+        }
+
+        return self::$countryRules;
+    }
 }
